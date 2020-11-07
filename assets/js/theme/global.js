e6d801162c6d0f700187d0d123bbf46c94a3190a
Logan Guthrie <logan@guthrie.solutions>
2020-11-06 18:29:46 -0800

    Mobile fixes

--- assets/js/theme/global.js
+++ assets/js/theme/global.js
@@ -38,10 +38,12 @@ export default class Global extends PageManager {
 
         // Accordion - Mobile Navigation
         (function ($) {
-            const allPanels = $('.accordion > dd').hide();
+            $('.accordion > dd').hide();
             // eslint-disable-next-line func-names
             $('.accordion > dt > a').click(function () {
-                if ($(this).parent().hasClass('active')) {
+                if ($(this).parent().hasClass('link-only')) {
+                    return;
+                } else if ($(this).parent().hasClass('active')) {
                     $(this).parent().next().slideUp();
                     $(this).parent().removeClass('active');
                 } else {
